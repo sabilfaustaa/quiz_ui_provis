e87e2fecc0e439cd2d7b059ff11274ecb36d932c
Muhamad Sabil Fausta <msabilfaustaa@gmail.com>
2024-03-29 20:46:47 +0700

    jawaban 1 oleh sabil

---
+++ android/app/src/main/kotlin/com/example/quiz_ui_provis/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.quiz_ui_provis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_ui_provis/jawaban_1.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Quiz UI Flutter',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Quiz UI Flutter'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            const Text(
+              'Nomor Kelompok:  26',
+            ),
+            const Text(
+              'Mhs 1:  2210142, Muhamad Sabil Fausta',
+            ),
+            const Text(
+              'Mhs 2:  2206272, Syaila Fathia Azzahra',
+            ),
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) {
+                    return soalNo1();
+                  }));
+                },
+                child: const Text('   Jawaban No 1   '),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) {
+                    return soalNo2();
+                  }));
+                },
+                child: const Text('   Jawaban No 2   '),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  //jaawaban no 1
+  Widget soalNo1() {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.amber[400],
+          actions: [],
+        ),
+        body: Jawaban1Page());
+  }
+
+  //jaawaban no 2
+  Widget soalNo2() {
+    return Scaffold(
+        // appBar: AppBar(
+        //   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // ),
+        body: Container());
+  }
+}
